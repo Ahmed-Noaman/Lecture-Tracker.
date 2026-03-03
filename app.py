e452afa19dfbcf22993eb92c63bac1b0c5fbfb23
Ahmed Noaman <89512716+Ahmed-Noaman@users.noreply.github.com>
2026-03-03 04:00:40 +0200

    Refactor app.py to restore import statements

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-%%writefile app.py
-
 import streamlit as st
 import pandas as pd
 from datetime import datetime
@@ -160,3 +158,4 @@ if submitted:
         sheet.append_row(new_row)
         st.success(f"✅ Created new record at {now}")
         
+
